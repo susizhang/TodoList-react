eea5ad085d894f9321a9cfa640ae82888bc5e972
suqun zhang <susizhang2017@gmail.com>
2022-10-21 18:15:42 +0200

    Updating localStorage file

--- src/App.js
+++ src/App.js
@@ -11,24 +11,10 @@ const App = () => {
     return localDate ? JSON.parse(localDate) : [];
   });
 
-  // run once when the app start
-  useEffect(() => {
-    getLocalTodos();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
   //  useEffect save to local
   useEffect(() => {
     localStorage.setItem("my-todos", JSON.stringify(todos));
   }, [todos]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  const getLocalTodos = () => {
-    if (localStorage.getItem("my-todos") === null) {
-      localStorage.setItem("my-todos", JSON.stringify([]));
-    } else {
-      let todoLocal = JSON.parse(localStorage.getItem("my-todos"));
-      setTodos(todoLocal);
-    }
-  };
 
   return (
     <>
